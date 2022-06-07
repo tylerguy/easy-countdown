a96855febb39ad983851aa4cba7e79b3470a8219
Tyler Denman <tylerdenman200@gmail.com>
2022-06-07 11:30:07 +0000

    remove particles.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import './App.css';
 import Home from './scenes/Home';
-import particlesJS from 'particles.js'
-
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/particlesjs-config.json', function() {
-  console.log('callback - particles.js config loaded');
-});
 
 function App() {
   return (
